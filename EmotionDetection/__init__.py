c190a7f6e0ddafcaab876d93d05e36c00c28d738
taykosok-dev <taykosok@gmail.com>
2026-05-16 15:23:35 -0400

    Commit for Project name and __init__.py

---
+++ EmotionDetection/__init__.py
@@ -0,0 +1,1 @@
+From emotion_detection import emotion_detector
---
+++ EmotionDetection/emotion_detection.py
@@ -0,0 +1,37 @@
+import requests
+import json
+
+def emotion_detector(text_to_analyze):
+    url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
+    headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
+    myobj = { "raw_document": { "text": text_to_analyze } }
+    response = requests.post(url, json=myobj, headers=headers)
+
+    if response.status_code!= 200:
+        return None
+
+    result = response.json()
+    emotions = result['emotionPredictions'][0]['emotion']
+
+    # Get individual scores
+    anger_score = emotions['anger']
+    disgust_score = emotions['disgust']
+    fear_score = emotions['fear']
+    joy_score = emotions['joy']
+    sadness_score = emotions['sadness']
+
+    # Find dominant emotion
+    dominant_emotion = max(emotions, key=emotions.get)
+
+    # Build output dict
+    output = {
+        'anger': anger_score,
+        'disgust': disgust_score,
+        'fear': fear_score,
+        'joy': joy_score,
+        'sadness': sadness_score,
+        'dominant_emotion': dominant_emotion
+    }
+
+    return output
+
